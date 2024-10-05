2dc9162362b6aa56866d821eb22874934d5967a5
gitshib123 <shibsobhan2004@gmail.com>
2024-10-05 12:40:25 +0530

    Q3 easy java Pixel Coders

--- Java/Easy/5.java
+++ Java/Easy/5.java
@@ -1,6 +1,10 @@
 class Main {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3};
-        System.out.println(arr);
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+        
     }
 }
