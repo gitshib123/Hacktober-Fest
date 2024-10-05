08164a392468cc8ce05a7c73f93afc38fe0c1d56
gitshib123 <shibsobhan2004@gmail.com>
2024-10-05 13:59:18 +0530

    84.py easy
    pixel coders

--- Python/Easy/84.py
+++ Python/Easy/84.py
@@ -2,7 +2,8 @@ def read_lines(filename):
   try:
     with open(filename, 'r') as file:
       lines = file.readlines()
+      return lines
   except FileNotFoundError:
     print('File not found')
-    return lines
+    
 result = read_lines('file.txt')
