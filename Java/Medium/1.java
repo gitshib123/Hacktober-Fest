ab811abeee306db4788c4a12c8acc3a762dd8639
gitshib123 <shibsobhan2004@gmail.com>
2024-10-05 12:49:17 +0530

    Q1 Java Medium Pixel Coders

--- Java/Medium/1.java
+++ Java/Medium/1.java
@@ -14,7 +14,7 @@ class Main {
             }
         }
         return -1;
-    }
+    } 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
         System.out.println(binarySearch(arr, 3));
