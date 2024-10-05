3b742662878a3c1b6412426bf2e929d15bddb4fd
gitshib123 <shibsobhan2004@gmail.com>
2024-10-05 12:28:07 +0530

    8.py
    pixel coders

--- Python/Easy/8.py
+++ Python/Easy/8.py
@@ -1,9 +1,12 @@
-if x = 5:
-    print("x is 5")
-if x == 5:
-    x = 10
+def int(x):
+
+    if x  == 5:
+        print("x is 5")
+
+    if x == 5:
+      x = 10
     print("x is now 10")
 
 x = 5
-if x = 5:
+if x == 5:
     print("x is 5")
