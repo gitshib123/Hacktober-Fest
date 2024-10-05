4df4d6cc152283da23346c1cd1ce3c39524044fd
gitshib123 <shibsobhan2004@gmail.com>
2024-10-05 14:02:13 +0530

    ;
    med64 python pixel coders

--- Python/Medium/64.py
+++ Python/Medium/64.py
@@ -1,8 +1,8 @@
 try:
- result = 10 / 0
- except ZeroDivisionError:
- print('Division by zero')
- else:
- print('No error')
- finally:
- print('Done')
+    result = 10 / 0
+except ZeroDivisionError:
+    print('Division by zero')
+else:
+    print('No error')
+finally:
+    print('Done')
